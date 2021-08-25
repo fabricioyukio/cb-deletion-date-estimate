c5c20b603057a09216f4893a620b810b721d9adb
Fabricio Yukio <fabricioyukio@gmail.com>
2021-08-25 13:58:20 -0300

    validating gold plan

--- lib/cb_deletion_date_estimate.rb
+++ lib/cb_deletion_date_estimate.rb
@@ -2,22 +2,51 @@ require 'date'
 
 class CbDeletionDateEstimate
   def self.is_due?(retention_plan, created_at)
+    retention_plan = retention_plan.upcase
+    raise ArgumentError unless (
+                                retention_plan.is_a?(String) &&
+                                ['STANDARD','GOLD','PLATINUM'].include?(retention_plan)
+                              )
+    raise ArgumentError unless (
+                                created_at.is_a?(Date) &&
+                                created_at <= Date.today
+                              )
+
+    # anyways, it should keep a snapshot at least for 42 days
     due_date = created_at + 42
     
     if ['GOLD','PLATINUM'].include?(retention_plan) then
-      due_date.year = due_date.year + 1
+      puts "GOLD OR PLATINUM"
+      due_date = due_date.next_year if self.last_of_the_month(created_at)
     end
 
+    if ['PLATINUM'].include?(retention_plan) then
+      if self.last_of_the_year(created_at) then
+        # it advances 6 years, instead of 7 because it already advanced 1 above
+        6.times do
+          due_date = due_date.next_year
+        end
+      end
+    end
 
+    today = Date.today
+    puts "today: #{today}"
+    puts "due_date: #{due_date}"
+    
+    (due_date < today)
   end
 
-  def last_of_the_month(dt)
+  def self.last_of_the_month(dt)
     dt_ = dt +1
-    return dt.month == dt_.month
+    # does month change by adding 1 day?
+    puts 'IS LAST_OF_THE_MONTH' if dt.month != dt_.month
+    puts 'IS NOT LAST_OF_THE_MONTH' if dt.month == dt_.month
+    return dt.month != dt_.month
   end
 
-  def last_of_the_year(dt)
-    dt_ = dt + ()
-    return dt.year == dt_.year
+  def self.last_of_the_year(dt)
+    dt_ = dt + 1
+    # does year change by adding 1 day?
+    return dt.year != dt_.year
   end
 end
--- spec/due_date_spec.rb
+++ spec/due_date_spec.rb
@@ -2,7 +2,42 @@ require 'rspec'
 require './spec/spec_helper.rb'
 
 describe CbDeletionDateEstimate do
-  it "validates inputs" do
-    expect(true).to be_truthy
-  end
+  describe ".is_due?" do
+    it "validates inputs" do
+      tomorrow = (Date.today) +1
+      yesterday = (Date.today) -1
+      existing_retention_plan = 'STANDARD'
+      non_existing_retention_plan = 'COPPER'
+
+      expect { CbDeletionDateEstimate.is_due?(non_existing_retention_plan,tomorrow) }.to raise_error(ArgumentError)
+      expect { CbDeletionDateEstimate.is_due?(non_existing_retention_plan, yesterday) }.to raise_error(ArgumentError)
+      expect { CbDeletionDateEstimate.is_due?(existing_retention_plan, yesterday) }.to_not raise_error
+    end
+
+    it 'calculates properly for Standard Retention Plan' do
+      due_date = (Date.today) - 43
+      undue_date = (Date.today) -1
+      retention_plan = 'StaNdard'
+
+      expect(CbDeletionDateEstimate.is_due?(retention_plan,due_date)).to be true
+      expect(CbDeletionDateEstimate.is_due?(retention_plan,undue_date)).to be false
+    end
+
+    it 'calculates properly for Gold Retention Plan' do
+      undue_date = (Date.today) -1
+      due_date = (Date.new(((undue_date.year) -1), (undue_date.month))) - 43
+      retention_plan = 'GOLd'
+
+      expect(CbDeletionDateEstimate.is_due?(retention_plan,due_date)).to be true
+      
+      expect(CbDeletionDateEstimate.is_due?(retention_plan,undue_date)).to be false
+
+      due_date = (Date.new(undue_date.year,undue_date.month).prev_month) -1
+      expect(CbDeletionDateEstimate.is_due?(retention_plan,due_date)).to be false
+      
+      due_date = (Date.new(undue_date.year,undue_date.month).prev_month) - 366
+      expect(CbDeletionDateEstimate.is_due?(retention_plan,due_date)).to be true
+    end
+end
+  
 end
